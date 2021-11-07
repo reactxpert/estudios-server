28a3f93ea584d96329f23a3889af0edeb4cb081c
Arpitkumar <arpit.daiict@gmail.com>
2021-11-07 21:53:42 +0530

    Auth0 Added

--- src/index.js
+++ src/index.js
@@ -2,12 +2,26 @@ import express from 'express'
 import cors from 'cors'
 import helmet from 'helmet'
 import morgan from 'morgan'
+import jwt from 'express-jwt'
+import jwksRsa from 'jwks-rsa'
 
 // defining the Express app
 const app = express()
 
 // defining port
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3000
+
+const jwtCheck = jwt({
+  secret: jwksRsa.expressJwtSecret({
+    cache: true,
+    rateLimit: true,
+    jwksRequestsPerMinute: 5,
+    jwksUri: `https://${process.env.AUTH0_DOMAIN}/.well-known/jwks.json`
+  }),
+  audience: process.env.AUTH0_API_IDENTIFIER,
+  issuer: `https://${process.env.AUTH0_DOMAIN}/`,
+  algorithms: ['RS256']
+})
 
 // adding Helmet to enhance your API's security
 app.use(helmet())
@@ -23,6 +37,11 @@ app.use(morgan('combined'))
 // defining an endpoint to return all ads
 app.get('/check', (req, res) => {
   res.send('Server is online!')
+})
+
+// This route needs authentication
+app.post('/authenticate', jwtCheck, function (req, res) {
+  res.send('You are successfully authenticated!')
 })
 
 // starting the server
