a528af9b09efbd84280d67644d0e6332477fc0c5
Arpitkumar <arpitkumar@reactxpert.com>
2021-11-07 20:13:22 +0530

    Initial Code

---
+++ src/index.js
@@ -0,0 +1,28 @@
+import express from 'express'
+import cors from 'cors'
+import helmet from 'helmet'
+import morgan from 'morgan'
+
+// defining the Express app
+const app = express()
+
+// adding Helmet to enhance your API's security
+app.use(helmet())
+
+app.use(express.json())
+
+// enabling CORS for all requests
+app.use(cors())
+
+// adding morgan to log HTTP requests
+app.use(morgan('combined'))
+
+// defining an endpoint to return all ads
+app.get('/check', (req, res) => {
+  res.send('Server is online!')
+})
+
+// starting the server
+app.listen(3001, () => {
+  console.log('listening on port 3001')
+})
